e09f1ab21a6366d1dd886ff31828b3f01fc70f27
Ino Tech <ezekieldavid150@gmail.com>
2025-10-01 00:42:59 +0100

    navbar styling

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,20 @@
 export default function Navbar() {
     return(
         <nav>
-    <img src="./src/assets/udeme-logo.png" alt="Udemility Logo" height={20}/>
-    </nav>
+            <div className="logo">
+                <img src="./src/assets/udeme-logo.png" alt="Udemility Logo"/>
+                <p className="udeme">Udemility <br /> <span className="elect">Electrical Services</span></p>
+            </div>
+            <ul className="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            {/* <a href="#" className="cta">Call Now</a> */}
+            <button className="cta"><img src="./src/assets/phone.svg" alt="Phone icon" /> Call Now</button>
+            
+        </nav>
     );
     
 }
